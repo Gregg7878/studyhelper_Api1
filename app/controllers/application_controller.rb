a0d27e83934702f800e1b9af9d14496f1efe46ff
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-09-13 17:15:02 +0300

    modified ApplicationController.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
-    #before_action :authorized, except: [:login ]
-  
+    before_action :authorized, except: [:login ]
+    
+
     def authorized
       return render json: { error: "Not Authorized" }, status: :unauthorized unless session.include?(:user_id)
     end
